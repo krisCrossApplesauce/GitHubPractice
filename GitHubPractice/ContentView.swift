989e221129d8f9934c8a34382360c15e112e13cc
PMStudent <PMStudent@RS-F251-M20.local>
2021-09-14 13:07:37 -0500

    this is a comment wrapped in quotes

---
+++ GitHubPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubPractice
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I am saying words")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubPractice/GitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticeApp.swift
+//  GitHubPractice
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
